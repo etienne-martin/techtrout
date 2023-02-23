fd445d35c9e9207d89b2ffa1c95c24469481bb77
Etienne <>
2023-02-23 14:38:31 -0500

    http headers

--- next.config.js
+++ next.config.js
@@ -2,4 +2,40 @@ const withNextra = require("nextra")({
   theme: "./src/theme.tsx",
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+  headers: async () => [
+    {
+      source: "/:path*",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "stale-while-revalidate=60, stale-if-error=3600, max-age=60",
+        },
+        {
+          key: "X-Frame-Options",
+          value: "DENY",
+        },
+        {
+          key: "X-XSS-Protection",
+          value: "0",
+        },
+        {
+          key: "Strict-Transport-Security",
+          value: "max-age=31536000; includeSubDomains",
+        },
+        {
+          key: "X-Content-Type-Options",
+          value: "nosniff",
+        },
+        {
+          key: "Referrer-Policy",
+          value: "origin-when-cross-origin",
+        },
+        {
+          key: "X-DNS-Prefetch-Control",
+          value: "on",
+        },
+      ],
+    },
+  ],
+});
