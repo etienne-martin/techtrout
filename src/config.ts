a8ace81e1a8a18c1a0ac8c69eb21bd92552188eb
Etienne <>
2023-02-24 15:08:51 -0500

    AdSense

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,5 @@ export const TWITTER_HANDLE = "techtrout";
 export const TWITTER_URL = `https://twitter.com/${TWITTER_HANDLE}`;
 
 export const GITHUB_URL = "https://github.com/etienne-martin/techtrout";
+
+export const GOOGLE_ADSENSE_CLIENT = "ca-pub-8127661126440950";
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { Provider } from "react-wrap-balancer";
 
 import { Layout } from "../components/layout/layout.component";
+import { GOOGLE_ADSENSE_CLIENT } from "../config";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider>
       <Layout>
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${GOOGLE_ADSENSE_CLIENT}`}
+          crossOrigin="anonymous"
+        />
         <Component {...pageProps} />
       </Layout>
     </Provider>
