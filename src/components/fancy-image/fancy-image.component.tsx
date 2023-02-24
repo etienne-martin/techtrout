1f91dfaa86304230fc9380dfb2c549e2c8b6757f
Etienne <>
2023-02-24 17:35:12 -0500

    priority image

--- src/components/fancy-image/fancy-image.component.tsx
+++ src/components/fancy-image/fancy-image.component.tsx
@@ -4,7 +4,7 @@ import type { ImageProps } from "next/dist/client/image";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 
-export const FancyImage: FC<ImageProps> = ({ src, alt }) => {
+export const FancyImage: FC<ImageProps> = ({ src, alt, ...otherProps }) => {
   return (
     <figure className="mt-10 -mx-4 sm:-mx-0">
       <div className="relative">
@@ -29,6 +29,7 @@ export const FancyImage: FC<ImageProps> = ({ src, alt }) => {
           <span className="absolute top-px bottom-px left-0 backdrop-invert backdrop-saturate-0 w-px" />
         </div>
         <Image
+          {...otherProps}
           src={src}
           alt={alt}
           placeholder="blur"
