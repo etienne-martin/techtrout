2b749d7686b2a9813dad37bcf16691b4e535098d
Etienne <>
2023-02-26 16:38:35 -0500

    uber story

--- src/components/quote/quote.component.tsx
+++ src/components/quote/quote.component.tsx
@@ -1,13 +1,20 @@
 import type { FC, HTMLAttributes } from "react";
 
+import { twMerge } from "tailwind-merge";
+
 import { Box } from "../box/box.component";
 
 export const Quote: FC<HTMLAttributes<HTMLElementTagNameMap["blockquote"]>> = ({
   children,
+  className,
   ...otherProps
 }) => {
   return (
-    <Box as="blockquote" {...otherProps}>
+    <Box
+      as="blockquote"
+      {...otherProps}
+      className={twMerge("italic", className)}
+    >
       {children}
     </Box>
   );
